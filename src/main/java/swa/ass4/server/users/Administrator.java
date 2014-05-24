b7c64c4330e4f3d5b320daa3b4e677afef8dd0ad
Pascal Attwenger <pascal.attwenger@gmail.com>
2014-05-24 21:16:24 +0200

    Users

---
+++ src/main/java/swa/ass4/server/users/Administrator.java
@@ -0,0 +1,7 @@
+package swa.ass4.server.users;
+
+public class Administrator extends User {
+	public Administrator(String firstName, String lastName, String userName, String password) {
+		super(firstName, lastName, userName, password);
+	}
+}
---
+++ src/main/java/swa/ass4/server/users/Student.java
@@ -0,0 +1,7 @@
+package swa.ass4.server.users;
+
+public class Student extends User {
+	public Student(String firstName, String lastName, String userName, String password) {
+		super(firstName, lastName, userName, password);
+	}
+}
